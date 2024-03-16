9aa2dbfc71c541f1645bcdb6c06455cff7d4460c
Anton <pakhorukov.a@mail.ru>
2024-03-16 22:20:54 +0700

    update

--- src/main/java/spring/min/work/controller/MainController.java
+++ src/main/java/spring/min/work/controller/MainController.java
@@ -16,7 +16,6 @@ import spring.min.work.service.EstimateService;
 import java.util.List;
 
 @Controller
-//@RequestMapping("/new")
 public class MainController {
     @Autowired
     private UserRepository userRepository;
@@ -28,8 +27,7 @@ public class MainController {
     private EstimateRepository estimateRepository;
 
     @GetMapping({"", "/"})
-    public String first(Model model) {
-//        model.addAttribute("name", );
+    public String first() {
         return "startPage";
     }
 
@@ -45,64 +43,60 @@ public class MainController {
     }
 
     @PostMapping("/mainPage")
-    public String checkAccount(@RequestParam("button") String buttonValue){
-        if("Logout".equals(buttonValue)){
+    public String checkAccount(@RequestParam("button") String buttonValue) {
+        if ("Logout".equals(buttonValue)) {
             return "login";
-        } else if ("Create Estimate".equals(buttonValue)){
+        } else if ("Create Estimate".equals(buttonValue)) {
             return "redirect:/createEstimate";
         } else {
             return "redirect:/details";
         }
     }
+
     @GetMapping("/details")
-    public String details(Model model){
+    public String details(Model model) {
         model.addAttribute("estimates", estimateService.getAll());
         return "details";
     }
+
     @GetMapping("/createEstimate")
-    public String createEstimate(Model model){
+    public String createEstimate(Model model) {
         model.addAttribute("estimates", estimateService.getAll());
         return "createEstimate";
     }
 
-
-//    @GetMapping("/createEstimate")
-//    public String back(Model model){
-//        model.addAttribute("estimates", estimateService.getAll());
-//        return "redirect:/mainPage";
-//    }
-
-@PostMapping("/createEstimate")
-    public String addEstimate(@RequestParam String room,
-                              @RequestParam String category,
-                              @RequestParam String description,
-                              @RequestParam String manufacturer,
-                              @RequestParam String product,
-                              @RequestParam String quantity,
-                              @RequestParam String price,
+    @PostMapping("/createEstimate")
+    public String addEstimate(@RequestParam String room, @RequestParam String category,
+                              @RequestParam String description, @RequestParam String manufacturer,
+                              @RequestParam String product, @RequestParam String quantity,
+                              @RequestParam String price, @RequestParam("button") String buttonValues,
                               Model model/*, User user*/) {
-        if (quantity.equals("") && !price.equals("")) {
-            quantity = "1";
-        } else if (quantity.equals("") && price.equals("")){
-            quantity = "1";
-            price = "1";
-            manufacturer = "test";
-            product = "test";
+        if ("Enter".equals(buttonValues)) {
+            System.err.println(buttonValues + " = add button");
+            if (quantity.equals("") && !price.equals("")) {
+                quantity = "1";
+            } else if (quantity.equals("") && price.equals("")) {
+                quantity = "1";
+                price = "1";
+                manufacturer = "test";
+                product = "test";
+            }
+            Estimate estimate = new Estimate(room, category, description,
+                    manufacturer, product, quantity, price);
+            estimateService.createEstimate(estimate);
+            List<Estimate> estimates = estimateService.getAll();
+            model.addAttribute("estimates", estimates);
+            return "createEstimate";
+        } else {
+            System.err.println("check enter button " + buttonValues);
+            model.addAttribute("estimates", estimateService.getAll());
+            return "redirect:/mainPage";
         }
-        Estimate estimate = new Estimate(room, category, description,
-                manufacturer, product, quantity, price);
-        System.out.println(estimate);
-        estimateService.createEstimate(estimate);
-        List<Estimate> estimates = estimateService.getAll();
-        System.out.println("list to html:");
-        estimates.stream().forEach(System.out::println);
-        model.addAttribute("estimates", estimates);
-        return "createEstimate";
     }
 
     @PostMapping("/createEstimate/deleteById")
-    public String deleteById(@RequestParam String delId, Model model){
-        if (delId != null && !delId.equals("")){
+    public String deleteById(@RequestParam String delId, Model model) {
+        if (delId != null && !delId.equals("")) {
             estimateService.deleteEstimateById(Integer.parseInt(delId));
             model.addAttribute("estimates", estimateService.getAll());
             return "createEstimate";
@@ -110,17 +104,20 @@ public class MainController {
         model.addAttribute("estimates", estimateService.getAll());
         return "createEstimate";
     }
+
     @PostMapping("/createEstimate/deleteAll")
-    public String deleteAllEstimate(Model model){
+    public String deleteAllEstimate(Model model) {
         estimateRepository.deleteAll();
         model.addAttribute("estimates", estimateService.getAll());
         return "createEstimate";
     }
 
     @PostMapping("/details")
-    public String backToMainPageFromDetails(){
+    public String backToMainPageFromDetails(Model model) {
+        model.addAttribute("estimates", estimateService.getAll());
         return "redirect:/mainPage";
     }
+
     @PostMapping("/main")
     public String addMessage(@RequestParam String text,
                              @RequestParam String tag,
@@ -153,7 +150,6 @@ public class MainController {
 
         }
         model.addAttribute("messages", messages);
-        System.out.println(messages);
         return path;
     }
 }
